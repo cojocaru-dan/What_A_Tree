a7ecc6dd7083f966ea63ce7aeb08322245f6aaaf
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-03-28 12:26:27 -0700

    Modify HTML using Javascript

--- client/public/script.js
+++ client/public/script.js
@@ -2,14 +2,25 @@ import { plants } from '/data.js';
 
 
 const divElement = function (content){
-  return `<div>${content}</div>`;
+  const speciesArr = content.species.split(", ");
+  let headers3 = "";
+  speciesArr.forEach(spec => headers3 += `<h3>${spec}</h3>`);
+  
+  return `<div>
+            <h2>${content.name}</h2>
+            ${headers3}
+          </div>`;
 }
 
 const loadEvent = function() {
 
   // Write your JavaScript code after this line
-
+  const rootElement = document.getElementById("root");
   
+  plants.forEach(plantObject => {
+    rootElement.insertAdjacentHTML("beforeend", divElement(plantObject));
+  });
+
   // Write your JavaScript code before this line
 
 }
